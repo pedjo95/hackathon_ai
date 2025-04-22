4df50ccf79faa9657586005b0d1548d249cf608b
fedora Cloud User <fedora@minha-vm-ai.novalocal>
2025-04-22 22:55:36 +0000

    first commit

---
+++ src/ai_news/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+from ai_news.crew import AiNews
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'outback',
+        'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    }
+
+    AiNews().crew().kickoff(inputs=inputs)
+
+run()
+    
+
